e6f17b021dfbd8919b816e7bc400304faf0d8032
Hossam Khair <hossam.khir1@outlook.com>
2020-01-11 14:36:14 +0200

    edited day01.java: missing code added

--- day01.java
+++ day01.java
@@ -4,13 +4,13 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class day1 {
-
+public class day01 {
+	
     public static void main(String[] args) {
         int i = 4;
         double d = 4.0;
         String s = "HackerRank ";
-
+		
         Scanner scan = new Scanner(System.in);
 
         /* Declare second integer, double, and String variables. */
@@ -25,15 +25,17 @@ public class day1 {
         sString = scan.nextLine();
         scan.reset();
         sString = scan.nextLine();
-
+        
         /* Print the sum of both integer variables on a new line. */
         System.out.println(i + u16Integer);
 
         /* Print the sum of the double variables on a new line. */
 		System.out.println(d + s32Double);
 
-        /* Concatenate and print the String variables on a new line;
+        /* Concatenate and print the String variables on a new line; 
         	the 's' variable above should be printed first. */
             System.out.println(s + sString);
 
         scan.close();
+    }
+}
